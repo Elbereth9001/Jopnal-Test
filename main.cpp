47eee4888e8cb69853147e1a82c07b8d21a243f8
Joona Tiinanen <joona_tiinanen@hotmail.com>
2016-02-26 15:05:43 +0200

    no message

--- main.cpp
+++ main.cpp
@@ -35,24 +35,32 @@ public:
 
     void initialize() override
     {
-        jop::Material def = jop::Material::getDefault();
-        def.setMap(jop::Material::Map::Diffuse, jop::ResourceManager::getResource<jop::Texture>("asdf"));
-
+        //auto attribs = jop::Material::DefaultAttributes;
+        auto attribs = jop::Material::Attribute::AmbientConstant | jop::Material::Attribute::Diffusemap | jop::Material::Attribute::Phong;
+        jop::Material def(attribs);
+        def.setShininess(5.f);
+        //def.setReflection(jop::Material::Reflection::Ambient, jop::Color::Green);
+        //def.setMap(jop::Material::Map::Diffuse, jop::ResourceManager::getResource<jop::Texture>("asdf"));
+        
         auto obj = createObject("Def");
-        obj->createComponent<jop::GenericDrawable>(*getDefaultLayer(), "defc");
+        obj->createComponent<jop::GenericDrawable>(*getDefaultLayer(), "defc")->setShader(jop::ShaderManager::getShader(attribs));
         obj->getComponent<jop::GenericDrawable>()->setModel(jop::Model(jop::ResourceManager::getNamedResource<jop::SphereMesh>("Ball", 1.f, 20, 20), def));
 
         obj->setPosition(0, 0, -3);
         obj->createComponent<SomeComponent>();
         auto rotator = obj->createChild("Rotator");
 
-        auto left = rotator->createChild("Left");
-        left->createComponent<jop::GenericDrawable>(*getDefaultLayer(), "DefDrawable");
-        left->setPosition(-1.5, 0, 0).setScale(0.5f);
+        auto left = rotator->createChild("Left")->createComponent<jop::LightSource>("Light");
+        left->setAttenuation(1.f, 0.7f, 1.8f, 5.f);
+        //left->setType(jop::LightSource::Type::Point);
+        //rotator->createChild("Left")->createComponent<jop::GenericDrawable>(*getDefaultLayer(), "DefDrawable");
+        rotator->createChild("Left")->setPosition(-10000.5, 0, -3);
+        
+        getDefaultLayer()->addLight(*left);
 
-        auto right = rotator->createChild("Right");
-        right->createComponent<jop::GenericDrawable>(*getDefaultLayer(), "DefDrawable");
-        right->setPosition(1.5, 0, 0).setScale(0.5f);
+        //auto right = rotator->createChild("Right");
+        //right->createComponent<jop::GenericDrawable>(*getDefaultLayer(), "DefDrawable");
+        //right->setPosition(1.5, 0, 0).setScale(0.5f);
 
         if (!jop::StateLoader::saveState("Scene/test", true, true, true))
             jop::Engine::exit();
@@ -63,13 +71,13 @@ public:
         const float sine = static_cast<float>(jop::Engine::getTotalTime()) * 4.f;
 
         getObject("Def")->rotate(0.f, dt, 0.f);
-        getObject("Def")->getChild("Rotator")->rotate(0.f, 0.f, -dt * 2.f);
+        //getObject("Def")->getChild("Rotator")->rotate(0.f, 0.f, -dt * 2.f);
 
-        auto l = getObject("Def")->getChild("Rotator")->getChild("Left");
-        auto r = getObject("Def")->getChild("Rotator")->getChild("Right");
+        ///*auto l = */getObject("Def")->getChild("Rotator")->getChild("Left")->move(2.f * dt * std::sin(sine), 2.f * dt * std::sin(sine), 0.f);
+        //auto r = getObject("Def")->getChild("Rotator")->getChild("Right");
 
-        l->setScale(0.3f * std::abs(std::sin(sine)) + 0.2f);
-        r->setScale(0.3f * std::abs(std::sin(sine)) + 0.2f);
+        //l->setScale(0.3f * std::abs(std::sin(sine)) + 0.2f);
+        //r->setScale(0.3f * std::abs(std::sin(sine)) + 0.2f);
     }
 };
 
@@ -109,17 +117,17 @@ int main(int c, char* v[])
         {
             if (key == jop::Keyboard::L)
                 jop::StateLoader::getInstance().loadState("Scene/test");
+            if (key == jop::Keyboard::Escape)
+                closed();
         }
     };
-
-    jop::ResourceManager::getResource<jop::Texture>("asdf");
 
     e.getSubsystem<jop::Window>()->setEventHandler<EventHandler>();
 
     e.createScene<SomeScene>();
 
-    for (int i = 1; i <= jop::Material::DefaultAttributes; ++i)
-        JOP_ASSERT(&jop::ShaderManager::getShader(i) != &jop::Shader::getDefault(), "aaa");
+    //for (int i = 1; i <= jop::Material::DefaultAttributes; ++i)
+    //    JOP_ASSERT(&jop::ShaderManager::getShader(i) != &jop::Shader::getDefault(), "aaa");
 
     return e.runMainLoop();
 }
