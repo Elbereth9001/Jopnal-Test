f3905205fc829fc20071d88707789b49a20a66e5
Joona Tiinanen <joona_tiinanen@hotmail.com>
2016-07-12 11:08:23 +0300

    no message

--- buildrun.bat
+++ buildrun.bat
@@ -1,6 +1,15 @@
 call android update project --target "android-18" --subprojects --path .
-call ndk-build
+call ndk-build NDK_DEBUG=1
+call :CHECK_FAIL
 call ant clean
 call ant debug
 call adb install -r bin/NativeActivity-debug.apk
 call run.bat
+
+:: /// check if the app has failed
+:CHECK_FAIL
+@echo off
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+    exit /b %errorlevel%
+)
